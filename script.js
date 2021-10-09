e6fc83c53d66f950e7d36f6ee872b864ec5e8fe4
Brane Marjanovic <marjanovicbrane93@gmail.com>
2021-10-09 23:49:20 +0200

    Here we are doing DOM manipulation.For now I created a game logic for guess number using random generated number between 1-20.We also display the message if we don't enter any number, if we enter correct number,if we enter too high number and if we enter too low number.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+
+//RANDOM NUMBER BETWEEN 1-20
+const secretNumber=Math.trunc(Math.random()*20)+1;
+//just for debugging
+document.querySelector('.number').textContent=secretNumber;
+
+//Adding event listener to the button check and saving into vairable value from the input field.
+document.querySelector('.check').addEventListener('click', function (){
+    const guess=Number(document.querySelector('.guess').value);
+    console.log(guess,typeof guess);
+
+    //When we don't enter anything to input field...
+    //guess=0 and o is falsy value, so guess is FALSE
+    //in this if statement.
+    //!FALSE=TRUE
+   if(!guess){
+    document.querySelector('.message').textContent='👎 No number!';
+   }else if(guess===secretNumber){
+    document.querySelector('.message').textContent='👍 Correct number!';
+   }else if(guess>secretNumber){
+    document.querySelector('.message').textContent='📈 Too high';
+   }else if(guess<secretNumber){
+    document.querySelector('.message').textContent='📉 Too low';
+   }
+})
